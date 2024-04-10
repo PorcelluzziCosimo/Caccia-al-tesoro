f9e501ea0814c2fd4623708dca5c13f6e2f62279
PorcelluzziCosimo <162311911+PorcelluzziCosimo@users.noreply.github.com>
2024-04-10 17:29:09 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -5,3 +5,21 @@ function doLogin() {
        alert("Username e password sono obbligatori");
    }
 }
+else {
+
+      fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+         }
+         body: JSON.stringify({
+            "username": username,
+            "password": password
+      })
+   })
+   .then(response => response.json())
+   .then(data => console.log(data))
+   .catch((error) => {
+      consol%error('Errore:', error);
+   });
+}
