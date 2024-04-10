354d7581dda14d9ae5040243c647cb9bb575eb46
PorcelluzziCosimo <162311911+PorcelluzziCosimo@users.noreply.github.com>
2024-04-10 17:34:18 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -13,8 +13,8 @@ else {
             'Content-Type': 'application/json',
          }
          body: JSON.stringify({
-            "username": username,
-            "password": password
+          "username": username,
+          "password": password
       })
    })
    .then(response => response.json())
